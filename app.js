21c8a57cbc6127f5ac70fc6990ab82b62ccad679
hawarhekmat <107219829+hawarhekmat@users.noreply.github.com>
2023-02-27 00:03:19 +0300

    make it responsive

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Make your future bright");
+const warning = document.querySelector(".warning");
+
+window.addEventListener("DOMContentLoaded", () => {
+  const showWarning = localStorage.getItem("showWarning");
+  if (showWarning) return;
+  warning.style.display = "flex";
+  warning.addEventListener("click", (e) => {
+    if (e.target.className === "warning" || e.target.tagName === "BUTTON") {
+      localStorage.setItem("showWarning", true);
+      warning.style.display = "none";
+    }
+  });
+});
